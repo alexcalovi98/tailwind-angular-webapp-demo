fc079dd172e65652abefca0bdc3aaf834b4f3261
Alex Calovi <alex.calovi98@gmail.com>
2021-10-24 11:01:02 +0200

    added login and panel components, very simple auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PanelComponent } from './panel/panel.component';
+import { AuthGuard  } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "", component: PanelComponent, canActivate: [AuthGuard] },
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
